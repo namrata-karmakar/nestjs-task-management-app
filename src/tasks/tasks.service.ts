752d22cec40211e30eb74e847abadb1192b49e1e
namrata-karmakar <namrata.krmkr@gmail.com>
2021-07-22 17:54:17 +0530

    created getTasks and updateTaskStatusById methods in repo

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -14,28 +14,9 @@ export class TasksService {
         private tasksRepository: TasksRepository
     ) {}
 
-
-
-    // getAllTasks(): Task[] {
-    //     return this.tasks;
-    // }
-
-    // getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
-    //     const { status, search } = filterDto;
-    //     let tasks = this.getAllTasks();
-    //     if (status) {
-    //         tasks = tasks.filter(task => task.status === status);
-    //     }
-    //     if (search) {
-    //         tasks = tasks.filter(task => {
-    //             if (task.title.includes(search) || task.description.includes(search)) {
-    //                 return true;
-    //             }
-    //             return false;
-    //         })
-    //     }
-    //     return tasks;
-    // }
+    getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+        return this.tasksRepository.getTasks(filterDto);
+    }
 
     async getTaskById(id: string): Promise<Task> {
         const found = await this.tasksRepository.findOne(id);
@@ -56,10 +37,11 @@ export class TasksService {
         }
     }
 
-    // updateTaskStatusById(id: string, status: TaskStatus): Task {
-    //     const task = this.getTaskById(id);
-    //     task.status = status;
-    //     return task;
-    // }
+    async updateTaskStatusById(id: string, status: TaskStatus): Promise<Task> {
+        const task = await this.getTaskById(id);
+        task.status = status;
+        await this.tasksRepository.save(task);
+        return task;
+    }
 
 }
